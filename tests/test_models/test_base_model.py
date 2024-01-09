fc0d933b86ea9076238afa32d13780381efb79cd
mugambi-victor <victormmugambi@gmail.com>
2024-01-09 10:58:13 +0300

    Adding unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import unittest
-from Models.base_model import BaseModel
+from unittest.mock import patch
+from models.base_model import BaseModel
+import uuid
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
 
@@ -17,7 +20,23 @@ class TestBaseModel(unittest.TestCase):
 
         # Check if created_at and updated_at are roughly equal (as they are initialized at the same time)
         self.assertAlmostEqual(instance.created_at.timestamp(), instance.updated_at.timestamp(), delta=0.01)
+    @patch('models.base_model.datetime')
+    def test_save_updates_updated_at(self, mock_datetime):
+        # Mock current datetime to control the test
+        mock_datetime.now.return_value = datetime(2024, 1, 8, 12, 0, 0)
 
+        # Create an instance of BaseModel
+        base_model = BaseModel()
+
+        # Set specific values for testing
+        base_model.updated_at = datetime(2023, 1, 8, 12, 0, 0)
+
+        # Call save() method
+        base_model.save()
+
+        # Assert that the updated_at attribute has been updated
+        expected_updated_at = datetime(2024, 1, 8, 12, 0, 0)
+        self.assertEqual(base_model.updated_at, expected_updated_at)
 
 if __name__ == '__main__':
     unittest.main()
