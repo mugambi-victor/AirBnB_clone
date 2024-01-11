d60749ee9ddb92468604d36139e77d410c00bde1
mugambi-victor <victormmugambi@gmail.com>
2024-01-11 12:19:55 +0300

    Updating console.py, added the following methods; all, create, destroy, show and update

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ form the prompt
 """
 
 import cmd
+import models
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -13,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb)'
     lastcmd = ""
+    valid_classes = ["BaseModel"]  # Add other valid classes as needed
 
     def do_quit(self, arg):
         "Quit command to exit the program"
@@ -22,10 +24,103 @@ class HBNBCommand(cmd.Cmd):
         'Exits'
         print()
         return True
-    
+
     def emptyline(self):
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = models.base_model.BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        print(models.storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        del models.storage.all()[key]
+        models.storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representations of all instances based on the class name."""
+        args = arg.split()
+        if args and args[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        instances = [str(value) for key, value in models.storage.all().items()]
+        print(instances)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating an attribute."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attribute_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attribute_value = args[3]
+        instance = models.storage.all()[key]
+        setattr(instance, attribute_name, attribute_value)
+        instance.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
