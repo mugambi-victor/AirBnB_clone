a1c07e1d8a3a5edf8958c06b0d280d52eddeb787
Solomon <kyobenimaru001@gmail.com>
2024-01-13 17:14:33 +0300

    commit fot the console 1.0 task

--- console.py
+++ console.py
@@ -7,6 +7,12 @@ form the prompt
 import cmd
 import models
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 
 class HBNBCommand(cmd.Cmd):
     """
